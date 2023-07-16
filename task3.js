c34f9274a4556c6dc385a999de38ad361fbd82ce
nadaelneel <80646472+nadaelneel@users.noreply.github.com>
2023-07-16 19:23:04 +0300

    task3.js

--- task3.js
+++ task3.js
@@ -1,8 +1,7 @@
 var age_in_year= parseInt(prompt("Enter Your Age"));
-var age_in_day= age_in_year * 30 * 12;
+var age_in_day= age_in_year * 365;
 if( age_in_day%2==0){
-    document.write("even");
+    document.write("("+age_in_year+") --> "+age_in_day+ "   even");
 }
 else{
-    document.write("odd");
-}
+    document.write("("+age_in_year+") -->"+age_in_day+ "    odd");
